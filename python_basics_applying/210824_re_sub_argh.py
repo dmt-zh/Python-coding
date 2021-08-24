9378f05bc230df9d4bca2c658e7b5fbb67250213
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-08-24 17:37:29 +0300

    Add files via upload

---
+++ python_basics_applying/210824_re_sub_argh.py
@@ -0,0 +1,19 @@
+# Вам дана последовательность строк. В каждой строке замените первое вхождение слова, состоящего только
+# из латинских букв "a" (регистр не важен), на слово "argh".
+
+# Примечание:
+# Обратите внимание на параметр count у функции sub.
+
+# Sample Input:
+# There’ll be no more "Aaaaaaaaaaaaaaa"
+# AaAaAaA AaAaAaA
+
+# Sample Output:
+# There’ll be no more "argh"
+# argh AaAaAaA
+
+import sys
+import re
+for line in sys.stdin:
+    line = line.rstrip()
+    print(re.sub(r'\b[Aa]+\b', 'argh', line, count=1))
