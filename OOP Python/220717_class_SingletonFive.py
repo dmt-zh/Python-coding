0f9e7c456a100332f2d203b5ac3c150cf7998b2e
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2022-07-17 14:54:19 +0300

    Add files via upload

---
+++ OOP Python/220717_class_SingletonFive.py
@@ -0,0 +1,21 @@
+# Объявите класс SingletonFive, с помощью которого можно было бы создавать объекты командой:
+# a = SingletonFive(<наименование>)
+# Здесь <наименование> - это данные, которые сохраняются в локальном свойстве name созданного объекта.
+# Этот класс должен формировать только первые пять объектов. Остальные (шестой, седьмой и т.д.) должны быть ссылкой
+# на последний (пятый) созданный объект.
+
+# Создайте первые десять объектов класса SingletonFive с помощью следующего фрагмента программы:
+# objs = [SingletonFive(str(n)) for n in range(10)]
+
+
+class SingletonFive:
+    __instance = None
+    __acc = 0
+    def __new__(cls, *args, **kwargs):
+        if cls.__acc < 5:
+            cls.__instance = super().__new__(cls)
+            cls.__acc += 1
+        return cls.__instance
+
+    def __init__(self, name):
+        self.name = name
