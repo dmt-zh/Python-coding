d998e6e8af71452a309de3049262d60c8e9f0ee7
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2022-08-29 12:50:34 +0300

    Add files via upload

---
+++ OOP Python/220829_class_Box3D(__add__, __sub__, __mul__).py
@@ -0,0 +1,57 @@
+# Объявите класс Box3D для представления прямоугольного параллелепипеда (бруска), объекты которого создаются командой:
+# box = Box3D(width, height, depth)
+# где width, height, depth - ширина, высота и глубина соответственно (числа: целые или вещественные)
+
+# В каждом объекте класса Box3D должны создаваться публичные атрибуты:
+# width, height, depth - ширина, высота и глубина соответственно.
+
+# С объектами класса Box3D должны выполняться следующие операторы:
+# box1 = Box3D(1, 2, 3)
+# box2 = Box3D(2, 4, 6)
+
+# box = box1 + box2 # Box3D: width=3, height=6, depth=9 (соответствующие размерности складываются)
+# box = box1 * 2    # Box3D: width=2, height=4, depth=6 (каждая размерность умножается на 2)
+# box = 3 * box2    # Box3D: width=6, height=12, depth=18
+# box = box2 - box1 # Box3D: width=1, height=2, depth=3 (соответствующие размерности вычитаются)
+# box = box1 // 2   # Box3D: width=0, height=1, depth=1 (соответствующие размерности целочисленно делятся на 2)
+# box = box2 % 3    # Box3D: width=2, height=1, depth=0
+# При каждой арифметической операции следует создавать новый объект класса Box3D с соответствующими значениями локальных атрибутов.
+
+
+
+
+class Box3D:
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+    @classmethod
+    def __valid_types(cls, value):
+        if not isinstance(value, (Box3D, int, float)):
+            raise ArithmeticError('Operand has to be of the type "Box3D", "int" or "float".')
+        return True
+
+    def __add__(self, other):
+        if self.__valid_types(other):
+            return Box3D(self.width + other.width, self.height + other.height, self.depth + other.depth)
+
+    def __sub__(self, other):
+        if self.__valid_types(other):
+            return Box3D(self.width - other.width, self.height - other.height, self.depth - other.depth)
+
+    def __mul__(self, other):
+        if self.__valid_types(other):
+            return Box3D(self.width * other, self.height * other, self.depth * other)
+
+    def __rmul__(self, other):
+        if self.__valid_types(other):
+            return self * other
+
+    def __floordiv__(self, other):
+        if self.__valid_types(other):
+            return Box3D(self.width // other, self.height // other, self.depth // other)
+
+    def __mod__(self, other):
+        if self.__valid_types(other):
+            return Box3D(self.width % other, self.height % other, self.depth % other)
