179c6cb1485ec477ab5f6dc9f28e4876c821f7cb
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2024-03-23 22:19:51 +0300

    Add files via upload

---
+++ threading/240324_ThreadPoolExecutor_future_wait.py
@@ -0,0 +1,23 @@
+# В тестирующей системе задан список ресурсов sources. Определена функция get_request_header, принимающая
+# в качестве аргумента элемент из списка ресурсов.
+
+# С помощью пула потоков создайте необходимое количество рабочих потоков, запустите задачи с использованием
+# метода submit(). Также реализуйте условие: если ответ от ресурса превышает 1,5 секунды, не дожидаться ответа
+# от ресурса. Вместо заголовков для ресурсов, ответ от которых превышает этот лимит, следует заполнять строковым
+# значением "no_response".
+
+
+
+import concurrent.futures
+
+headers_stor = {}
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    future_works = {executor.submit(get_request_header, url): url for url in sources}
+    done, not_done = concurrent.futures.wait(future_works.keys(), timeout=1.5)
+    for future in done:
+        value = future_works.get(future)
+        headers_stor[value] = future.result()
+    for future in not_done:
+        value = future_works.get(future)
+        headers_stor[value] ='no_response'
